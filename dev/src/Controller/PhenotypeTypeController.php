fb8a7cc71d2d25c1f56b96c6d2d5c8e341dd053b
Ngaudin13 <nicolas.gaudin@etu.univ-lyon1.fr>
2025-09-04 17:17:42 +0200

    fix[] : mise en place controleur et vue datatable pour phneotypeType

--- dev/src/Controller/PhenotypeTypeController.php
+++ dev/src/Controller/PhenotypeTypeController.php
@@ -3,20 +3,21 @@
 namespace App\Controller;
 
 use App\Entity\PhenotypeType;
+use App\Entity\Phenotype;
 use App\Form\PhenotypeTypeFormType;
 use App\Repository\PhenotypeTypeRepository;
 use App\Repository\PhenotypeTypeRepositoryInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use FOS\ElasticaBundle\Finder\PaginatedFinderInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Knp\Component\Pager\PaginatorInterface;
-use FOS\ElasticaBundle\Finder\PaginatedFinderInterface;
 
 class PhenotypeTypeController extends AbstractController
 {
@@ -28,62 +29,189 @@ class PhenotypeTypeController extends AbstractController
     ) {
     }
 
-    #[Route(path: 'page_phenotypetypes', name: 'page_phenotypetypes')]
+    #[Route(path: 'strains/page_phenotypetypes', name: 'page_phenotypetypes')]
     #[IsGranted('ROLE_ADMIN')]
     public function showPage(Request $request, EntityManagerInterface $em, Security $security): Response
     {
-        $phenotypetype = new PhenotypeType();
-        $phenotypetypeForm = $this->createForm(PhenotypeTypeFormType::class, $phenotypetype);
-        $phenotypetypeForm->handleRequest($request);
+        // même pattern que ProjectController::showPage
+        $phenotypeTypeAdd = $this->createForm(PhenotypeTypeFormType::class);
 
-        if ($phenotypetypeForm->isSubmitted() && $phenotypetypeForm->isValid()) {
-            $em->persist($phenotypetype);
-            $em->flush();
+        // traitement du submit via addForm()
+        $phenotypeTypeAdd = $this->addForm($request, $em);
 
-            $this->addFlash('success', 'Phenotype type ' . $phenotypetype->getType() . ' added with success!');
-            return $this->redirectToRoute('page_phenotypetypes');
-        }
-
-        $types = $this->phenotypetypeRepository->findAll();
+        $allTypes = $this->phenotypetypeRepository->findAll();
 
         return $this->render('phenotypeType/main.html.twig', [
-            'phenotypetypeForm' => $phenotypetypeForm->createView(),
-            'types' => $types
+            // IMPORTANT : clé attendue par tes templates + FormView
+            'phenotypeTypeForm' => $phenotypeTypeAdd->createView(),
+            'types' => $allTypes,
         ]);
     }
 
-    #[Route('phenotypetype/edit/{id}', name: 'edit_phenotypetype')]
+    #[Route(path: 'strains/phenotypetype/ajout', name: 'add_phenotypetype')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function addForm(Request $request, EntityManagerInterface $em): Form
+    {
+        $type = new PhenotypeType();
+
+        $form = $this->createForm(PhenotypeTypeFormType::class, $type);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($type);
+            $em->flush();
+            // on retourne le Form (même pattern que ProjectController)
+            return $form;
+        }
+
+        return $form;
+    }
+
+    #[Route(path: 'strains/phenotypetype/ajout/response', name: 'add_phenotypetype_response')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function addResponse(Request $request, EntityManagerInterface $em): Response
+    {
+        $type = new PhenotypeType();
+
+        $form = $this->createForm(PhenotypeTypeFormType::class, $type);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($type);
+            $em->flush();
+            return $this->redirectToRoute('page_phenotypetypes');
+        }
+
+        // IMPORTANT : passer phenotypeTypeForm => FormView
+        return $this->render('phenotypeType/create.html.twig', [
+            'phenotypeTypeForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('strains/phenotypetype/edit/{id}', name: 'edit_phenotypetype')]
     #[IsGranted('ROLE_ADMIN')]
     public function edit(
         PhenotypeType $phenotypetype,
         Request $request,
         EntityManagerInterface $em,
-    ): Response {
+        Security $security
+    ): Response
+    {
+        if (!$security->isGranted('ROLE_ADMIN')) {
+            $this->addFlash('error', 'You do not have permission to edit a phenotype type.');
+            return $this->redirectToRoute('page_phenotypetypes');
+        }
 
-        $phenotypetypeForm = $this->createForm(PhenotypeTypeFormType::class, $phenotypetype);
+        $form = $this->createForm(PhenotypeTypeFormType::class, $phenotypetype);
+        $form->handleRequest($request);
 
-        $phenotypetypeForm->handleRequest($request);
-
-        if ($phenotypetypeForm->isSubmitted() && $phenotypetypeForm->isValid()) {
-
+        if ($form->isSubmitted() && $form->isValid()) {
             $em->persist($phenotypetype);
             $em->flush();
 
-            $this->addFlash('success', 'Phenotype type ' . $phenotypetype->getType() . ' modified with success!');
-
+            $this->addFlash('success', 'Phenotype type "' . $phenotypetype->getType() . '" modified with success!');
             return $this->redirectToRoute('page_phenotypetypes');
         }
-        return $this->render('phenotypetype/edit.html.twig', compact('phenotypetypeForm'));
+
+        // IMPORTANT : passer phenotypeTypeForm => FormView
+        return $this->render('phenotypeType/edit.html.twig', [
+            'phenotypeTypeForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('phenotypetypes/duplicate/{id}', name: 'duplicate_phenotypetype')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function duplicate(PhenotypeType $phenotypetype, EntityManagerInterface $em, Security $security): Response
+    {
+        try {
+            if (!$security->isGranted('ROLE_ADMIN')) {
+                $this->addFlash('error', 'You do not have permission to duplicate a phenotype type.');
+                return $this->redirectToRoute('page_phenotypetypes');
+            }
+
+            $clone = new PhenotypeType();
+            $clone->setType($phenotypetype->getType());
+
+            $em->persist($clone);
+            $em->flush();
+
+            $this->addFlash('success', 'Phenotype type "' . $clone->getType() . '" duplicated successfully!');
+            return $this->redirectToRoute('page_phenotypetypes');
+        } catch (\Throwable $e) {
+            $this->addFlash('error', 'Error occurred while duplicating the phenotype type.');
+            return $this->redirectToRoute('page_phenotypetypes');
+        }
     }
 
     #[Route('strains/phenotypetype/delete/{id}', name: 'delete_phenotypetype')]
     #[IsGranted('ROLE_ADMIN')]
-    public function delete(phenotypetype $phenotypetype, EntityManagerInterface $em): Response
+    public function delete(PhenotypeType $phenotypetype, EntityManagerInterface $em, Security $security): Response
     {
-        $em->remove($phenotypetype);
-        $em->flush();
+        if (!$security->isGranted('ROLE_ADMIN')) {
+            $this->addFlash('error', 'You do not have permission to delete a phenotype type.');
+            return $this->redirectToRoute('page_phenotypetypes');
+        }
 
-        $this->addFlash('success', 'Phenotype type ' . $phenotypetype->getType() . ' deleted with success!');
+        // bloquer si des Phenotype y sont liés
+        $linkedCount = $em->getRepository(Phenotype::class)->count(['phenotypeType' => $phenotypetype]);
+
+        if ($linkedCount > 0) {
+            $this->addFlash('error', sprintf(
+                'Cannot delete this phenotype type because it is associated with %d phenotype(s).',
+                $linkedCount
+            ));
+        } else {
+            $em->remove($phenotypetype);
+            $em->flush();
+            $this->addFlash('success', 'Phenotype type "' . $phenotypetype->getType() . '" has been successfully deleted!');
+        }
+
+        return $this->redirectToRoute('page_phenotypetypes');
+    }
+
+    #[Route('/phenotypetypes/delete-multiple', name: 'delete_multiple_phenotypetypes', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function deleteMultiple(Request $request, EntityManagerInterface $em): Response
+    {
+        $ids = $request->request->all('selected_types');
+
+        if (!is_array($ids) || empty($ids)) {
+            $this->addFlash('error', 'No phenotype type selected.');
+            return $this->redirectToRoute('page_phenotypetypes');
+        }
+
+        $types = $em->getRepository(PhenotypeType::class)->findBy(['id' => $ids]);
+
+        if (!$types) {
+            $this->addFlash('error', 'No phenotype type found for deletion.');
+            return $this->redirectToRoute('page_phenotypetypes');
+        }
+
+        $detailsDeleted = [];
+        $detailsBlocked = [];
+
+        foreach ($types as $type) {
+            $linkedCount = $em->getRepository(Phenotype::class)->count(['phenotypeType' => $type]);
+            if ($linkedCount > 0) {
+                $detailsBlocked[] = sprintf('[ID: %d - Type: %s]', $type->getId(), $type->getType());
+                continue;
+            }
+            $detailsDeleted[] = sprintf('[ID: %d - Type: %s]', $type->getId(), $type->getType());
+            $em->remove($type);
+        }
+
+        if (!empty($detailsDeleted)) {
+            $em->flush();
+            $this->addFlash('success', sprintf(
+                '%d phenotype type(s) successfully deleted: %s',
+                count($detailsDeleted),
+                implode(', ', $detailsDeleted)
+            ));
+        }
+
+        if (!empty($detailsBlocked)) {
+            $this->addFlash('error', 'Unable to delete some phenotype types because they are linked to phenotypes: ' . implode(', ', $detailsBlocked));
+        }
 
         return $this->redirectToRoute('page_phenotypetypes');
     }
