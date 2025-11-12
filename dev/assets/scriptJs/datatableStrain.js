5a0ede8628138750be4e964db7e4c5564330562c
Ngaudin13 <nicolas.gaudin@etu.univ-lyon1.fr>
2025-11-12 19:00:30 +0100

    filtre sur collone 4 5 6 , et modification css datatable strain

--- dev/assets/scriptJs/datatableStrain.js
+++ dev/assets/scriptJs/datatableStrain.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
         ],
         order: [],
         columnDefs: [
-            { orderable: false, targets: [0,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22] },
+            { orderable: false, targets: [0,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22] },
         {
         targets: 3,  // convertion pour le trie sur date
             render: function (data, type) {
