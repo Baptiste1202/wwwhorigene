fdb4b1a6485f0aff64305c4edb07f0b1c56c021c
Baptiste1202 <154373956+Baptiste1202@users.noreply.github.com>
2025-11-16 21:05:24 +0100

    Update Version20251115190341.php

--- dev/migrations/Version20251115190341.php
+++ dev/migrations/Version20251115190341.php
@@ -22,11 +22,6 @@ final class Version20251115190341 extends AbstractMigration
         // Modifier la colonne 'date' de drug_resistance_on_strain avec conversion explicite
         $this->addSql('ALTER TABLE drug_resistance_on_strain ALTER COLUMN date TYPE TIMESTAMP USING date::timestamp');
         $this->addSql('ALTER TABLE drug_resistance_on_strain ALTER COLUMN date DROP DEFAULT');
-
-        // Modifier la colonne 'is_verified' de user en 'is_access' de type BOOLEAN
-        $this->addSql('ALTER TABLE "user" ALTER COLUMN is_verified TYPE BOOLEAN USING is_verified::boolean');
-        $this->addSql('ALTER TABLE "user" RENAME COLUMN is_verified TO is_access');
-        $this->addSql('ALTER TABLE "user" ALTER COLUMN is_access SET NOT NULL');
     }
 
     public function down(Schema $schema): void
@@ -34,10 +29,5 @@ final class Version20251115190341 extends AbstractMigration
         // Revenir sur la colonne 'date' avec conversion explicite
         $this->addSql('ALTER TABLE drug_resistance_on_strain ALTER COLUMN date TYPE VARCHAR(255) USING date::varchar');
         $this->addSql('ALTER TABLE drug_resistance_on_strain ALTER COLUMN date DROP DEFAULT');
-
-        // Revenir sur la colonne 'is_access' vers 'is_verified' de type BOOLEAN
-        $this->addSql('ALTER TABLE "user" ALTER COLUMN is_access TYPE BOOLEAN USING is_access::boolean');
-        $this->addSql('ALTER TABLE "user" RENAME COLUMN is_access TO is_verified');
-        $this->addSql('ALTER TABLE "user" ALTER COLUMN is_verified SET NOT NULL');
     }
 }
