c404ca64a38fd365ecce16868dd496b0ff7ff12d
Baptiste1202 <154373956+Baptiste1202@users.noreply.github.com>
2025-11-16 20:57:56 +0100

    Update Version20251115190341.php

--- dev/migrations/Version20251115190341.php
+++ dev/migrations/Version20251115190341.php
@@ -19,8 +19,8 @@ final class Version20251115190341 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // Modifier la colonne 'date' de drug_resistance_on_strain
-        $this->addSql('ALTER TABLE drug_resistance_on_strain ALTER COLUMN date TYPE TIMESTAMP');
+        // Modifier la colonne 'date' de drug_resistance_on_strain avec conversion explicite
+        $this->addSql('ALTER TABLE drug_resistance_on_strain ALTER COLUMN date TYPE TIMESTAMP USING date::timestamp');
         $this->addSql('ALTER TABLE drug_resistance_on_strain ALTER COLUMN date DROP DEFAULT');
 
         // Modifier la colonne 'is_verified' de user en 'is_access' de type BOOLEAN
@@ -31,8 +31,8 @@ final class Version20251115190341 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // Revenir sur la colonne 'date'
-        $this->addSql('ALTER TABLE drug_resistance_on_strain ALTER COLUMN date TYPE VARCHAR(255)');
+        // Revenir sur la colonne 'date' avec conversion explicite
+        $this->addSql('ALTER TABLE drug_resistance_on_strain ALTER COLUMN date TYPE VARCHAR(255) USING date::varchar');
         $this->addSql('ALTER TABLE drug_resistance_on_strain ALTER COLUMN date DROP DEFAULT');
 
         // Revenir sur la colonne 'is_access' vers 'is_verified' de type BOOLEAN
