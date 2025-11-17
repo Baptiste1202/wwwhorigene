3473bbebb6edb83af097afd3e7a1a57d2d5d0c4e
Baptiste1202 <154373956+Baptiste1202@users.noreply.github.com>
2025-11-17 17:39:19 +0100

    Update populate.sh

--- populate.sh
+++ populate.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Aller dans le dossier du projet (ajuste le chemin si besoin)
-docker exec -it claranet2-app-1 bash
-
 # Lancer la commande fos:elastica:populate
 php bin/console fos:elastica:populate
