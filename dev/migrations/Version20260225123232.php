3cc40b6833273d6255639bc3fe442a9a9cc5e0db
Baptiste1202 <154373956+Baptiste1202@users.noreply.github.com>
2026-02-26 20:27:28 +0100

    Rm unused UNSIGNED

--- dev/migrations/Version20260225123232.php
+++ dev/migrations/Version20260225123232.php
@@ -19,20 +19,6 @@ final class Version20260225123232 extends AbstractMigration
         // 1️⃣ Nouvelle colonne
         $this->addSql('ALTER TABLE drug_resistance_on_strain ADD concentration_unit VARCHAR(50) DEFAULT NULL');
 
-        // 2️⃣ Aligner method_sequencing_type.id en INT UNSIGNED
-        $this->addSql('ALTER TABLE method_sequencing_type CHANGE id id INT UNSIGNED AUTO_INCREMENT NOT NULL');
-
-        // 3️⃣ Aligner sequencing.id et sequencing.name_id en INT UNSIGNED
-        $this->addSql('ALTER TABLE sequencing 
-            CHANGE id id INT UNSIGNED AUTO_INCREMENT NOT NULL,
-            CHANGE size_file size_file INT DEFAULT NULL,
-            CHANGE name_id name_id INT UNSIGNED DEFAULT NULL
-        ');
-
-        // 4️⃣ FK sample → user
-        $this->addSql('CREATE INDEX IDX_F10B76C3A76ED395 ON sample (user_id)');
-        $this->addSql('ALTER TABLE sample ADD CONSTRAINT FK_F10B76C3A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-
         // 5️⃣ Favoris
         $this->addSql('ALTER TABLE strain ADD is_favorite TINYINT(1) DEFAULT 0 NOT NULL');
     }
